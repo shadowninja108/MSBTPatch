4a54d3b30e3a96901c330a7a0e1b6cf109b40b4e
shadowninja108 <shadowninja0108@gmail.com>
2018-07-11 00:41:38 -0700

    Debug -> Console

--- MSBTPatch/Program.cs
+++ MSBTPatch/Program.cs
@@ -23,18 +23,18 @@ namespace MSBTPatch
             foreach (KeyValuePair<long, Entry> kv in msbt.entries)
             {
                 Entry entry = kv.Value;
-                Debug.WriteLine($"{entry.key} : {entry.value}");
+                Console.WriteLine($"{entry.key} : {entry.value}");
                 if (entry.attributes != null)
                 {
                     Attributes a = entry.attributes;
-                    Debug.WriteLine($"Attributes:\nUnk1: {a.unk1}\nUnk2: {a.unk2}\nType: {a.type}\nUnk3: {a.unk3}\nUnk4: {a.unk4}\nUnk5: {a.unk5}\nUnk6: {a.unk6}\nUnk7: {BitConverter.ToString(a.unk7)}");
+                    Console.WriteLine($"Attributes:\nUnk1: {a.unk1}\nUnk2: {a.unk2}\nType: {a.type}\nUnk3: {a.unk3}\nUnk4: {a.unk4}\nUnk5: {a.unk5}\nUnk6: {a.unk6}\nUnk7: {BitConverter.ToString(a.unk7)}");
                 } else
                 {
-                    Debug.WriteLine("No attributes.");
+                    Console.WriteLine("No attributes.");
                 }
             }
 
-            Console.ReadKey();
+            Console.ReadKey(); // just wait until the user does something so i can actually see the output
         }
     }
 }
